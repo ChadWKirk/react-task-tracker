4fb553e06f19732856c8b98599a715b12142ed21
Chad Kirk <santaomgprod@gmail.com>
2022-08-02 21:17:31 -0500

    learning useState()

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
 const Tasks = () => {
-  return <div>Tasks</div>;
+  const [tasks, setTasks] = useState([
+    {
+      text: "1",
+      id: "1",
+    },
+    {
+      text: "2",
+      id: "2",
+    },
+    {
+      text: "3",
+      id: "3",
+    },
+  ]);
+
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
