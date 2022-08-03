159a9d8a9d34baf6807c7e8eec50f04060a388a1
Chad Kirk <santaomgprod@gmail.com>
2022-08-02 20:47:17 -0500

    prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './components/Header'
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="container">
-      <Header title='Task Tracker' />
+      <Header title="Task Tracker" />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
-import Button from "./Button"
+import Button from "./Button";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
   const onClick = () => {
-    console.log('click')
-  }
+    console.log("click");
+  };
 
   return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
