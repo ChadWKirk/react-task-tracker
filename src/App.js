80c56f2a7f6c8f44803b443570bd158f15df2fa6
Chad Kirk <ChadWKirk@gmail.com>
2022-08-04 14:07:55 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -8,16 +9,19 @@ const App = () => {
       text: "1",
       day: "today",
       id: "1",
+      reminder: true,
     },
     {
       text: "2",
       day: "tomorrow",
       id: "2",
+      reminder: true,
     },
     {
       text: "3",
       day: "the next day",
       id: "3",
+      reminder: true,
     },
   ]);
 
@@ -26,11 +30,21 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <AddTask />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
